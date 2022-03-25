e5fbd496caf9c4d4a7cad963834e8cec41590179
dongsu <sonicce99@naver.com>
2022-03-25 17:58:42 +0900

    docs : details router 추가

---
+++ pages/details.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Details: NextPage = () => {
+  return (
+    <>
+      <nav aria-label="fastcampus">fastcampus</nav>
+      <div role="banner">banner</div>
+      <div title="lectureList">lectureList</div>
+    </>
+  );
+};
+
+export default Details;
